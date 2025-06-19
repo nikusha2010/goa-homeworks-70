d6c753db00284fa96f1dcd8dfd8b29d048d406e6
nikusha2010 <soupsp1110@gmail.com>
2025-06-19 14:06:46 +0400

    Initial Commit

---
+++ level 15/homework/hw5.py
@@ -0,0 +1,8 @@
+# 5) შექმენი ლისტი, შეინახეთ სამი წიგნის სახელი. დაბეჭდე მთელი სია და დაბეჭდე, რამდენი წიგნია მასში.
+
+names = ["Pride and Prejudice", "1984", "Crime and Punishment"]
+
+print(names)
+
+for i in range(len(names)):
+    print(i)                                                                                                                    
